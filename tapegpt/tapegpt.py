bebd2ca70e0ad1791763c944c3755ed8e832e0dc
user <51804240+zielo-hue@users.noreply.github.com>
2023-02-20 18:26:08 -0800

    the transformer blocc

--- tapegpt/tapegpt.py
+++ tapegpt/tapegpt.py
@@ -85,7 +85,6 @@ class Head(nn.Module):
         out = wei @ v  # (B, T, T) @ (B, T, C) => (B, T, C)
         return out
 
-
 class MultiHeadAttention(nn.Module):
     """A Bunch Of Attention Heads!!! A bit like group convolution"""
 
@@ -95,7 +94,6 @@ class MultiHeadAttention(nn.Module):
     
     def forward(self, x: torch.Tensor):
         return torch.cat([head(x) for head in self.heads], dim=-1)
-
 
 class FeedForward(nn.Module):
     """The 'Feed Forward Network' part of the Transformer (also multilayered perceptron)"""
@@ -107,6 +105,20 @@ class FeedForward(nn.Module):
     def forward(self, x: torch.Tensor):
         return self.net(x)
 
+class Block(nn.Module):
+    """The Transformer Block: attention (communication) then computation"""
+
+    def __init__(self, n_embed, n_head):
+        super().__init__()
+        head_size = n_embed // n_head
+        self.self_attention = MultiHeadAttention(n_head, head_size)
+        self.feed_foward = FeedForward(n_embed)
+    
+    def forward(self, x: torch.Tensor):
+        x = self.self_attention(x)
+        x = self.feed_foward(x)
+        return x
+
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
@@ -114,8 +126,11 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.attention_heads = MultiHeadAttention(4, n_embed//4)  # 4 heads, and each head is smaller than a single Head config
-        self.feed_forward = FeedForward(n_embed)
+        self.blocks = nn.Sequential(
+            Block(n_embed, n_head=4),
+            Block(n_embed, n_head=4),
+            Block(n_embed, n_head=4)
+        )
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx: torch.Tensor, targets=None):
@@ -125,8 +140,7 @@ class BigramLanguageModel(nn.Module):
         token_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T, C)
         x = token_emb + pos_emb  # (B, T, C) b/c added dimension -> broadcasted across each batch
-        x = self.attention_heads(x)  # apply head of self attention (B, T, C)
-        x = self.feed_forward(x)  # (B, T, C) now with attention computed, use attention information
+        x = self.blocks(x)  # (B, T, C) now with attention computed, use attention information
         logits = self.lm_head(x)  # (B,T,vocab_size)
 
         if targets is None:
